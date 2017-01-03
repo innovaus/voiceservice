ac8c32a56cfe6b3240ba46b7e5fa9d2bdee7be51
Meganathan, Sathiskumar <sathiskumar.meganathan@usbank.com>
2017-01-03 14:11:42 -0800

    change in request body logging into console

--- routes/routes.js
+++ routes/routes.js
@@ -93,7 +93,7 @@ app.post("/branchalexa", function(req, res) {
 
 
   app.post("/branchlocator", function(req, res) {
-    console.log("Request=" + req);
+    console.log("Request=" + req.body);
       var zip = req.body.result.parameters.zipcode;
       if(zip == null || zip == "" || zip.length < 5 || zip.length > 5){
         var branchResponse =
